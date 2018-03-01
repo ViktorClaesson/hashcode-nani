e2c95625d1a34c821bc287aada231ffb46c03933
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2018-03-01 21:08:55 +0100

    rem

--- src/main/java/utils/RideCalc.java
+++ src/main/java/utils/RideCalc.java
@@ -15,11 +15,13 @@ public class RideCalc {
         Point carLocation = car.lastPosition();
         int atTime = car.lastTime;
 
+        //Har vi redan missat riden?
         List<Ride> possibleRides = rides.stream().filter(x -> x.getTimeSpan().getFinish() >= atTime).collect(Collectors.toList());
 
         Ride bestRide = null;
         int bestRideTime = Integer.MAX_VALUE;
         for (Ride ride : possibleRides) {
+            //
             int rideTime = carLocation.distanceTo(ride.getFrom());
             boolean canGetInTime = ride.getTimeSpan().withinSpan(atTime + rideTime);
             if (canGetInTime && rideTime < bestRideTime) {
@@ -29,6 +31,7 @@ public class RideCalc {
         if (bestRide == null) {
             return false;
         }
+        rides.remove(bestRide);
         car.currentPosition = bestRide.getTo();
         car.addPassenger(bestRide, bestRideTime + atTime);
         return true;
