b845a39f3c1a124d270d4afb6067da0c352f7944
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2018-03-01 18:55:44 +0100

    point

---
+++ Point.java
@@ -0,0 +1,44 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Point {
+    private int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int hashCode() {
+        return ((x + y) * (x + y + 1) )/ 2 + y;
+    }
+
+    public List<Point> getNeighbours() {
+        List<Point> neighbours = new ArrayList<>(8);
+        for (int i = x - 1; i <= x + 1; i++) {
+            for (int j = y - 1; j <= y + 1; j++) {
+                if (!(x == i && y == j)) {
+                    neighbours.add(new Point(i, j));
+                }
+            }
+        }
+        return neighbours;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return this.hashCode() == obj.hashCode();
+    }
+
+    public boolean insideBoard(int size) {
+        return (x >= 0 && x < size) && (y >= 0 && y < size);
+    }
+
+    @Override
+    public String toString() {
+        return "X,Y (" + x + "," + y + ")";
+    }
+}
